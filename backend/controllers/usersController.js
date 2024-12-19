1ea9d4d91e345bda7dba8627adb976b810e15bbc
Georgi Goranov <124670356+GeorgiGoranov@users.noreply.github.com>
2024-12-19 15:16:02 +0100

    Update usersController.js

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -108,7 +108,7 @@ const getUserLogin = async (req, res) => {
 
         const token = createToken(user._id, user.role, user.username);
 
-        res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000,sameSite: 'None',secure: true, path: '/',}); // cookie operates in milisecond and not in minutes
+        res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000,sameSite: 'None',secure: true }); // cookie operates in milisecond and not in minutes
 
         console.log(token)
 
