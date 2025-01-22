85605afd4bc1af0068098ca118bb3062914a1a80
Georgi Goranov <124670356+GeorgiGoranov@users.noreply.github.com>
2025-01-22 19:06:03 +0100

    fontys logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { useLanguage } from '../context/LanguageContext';
 const API_URL = process.env.REACT_APP_BACKEND_URL;
 
 
+
 const NavBar = () => {
   const [isLoggingOut, setIsLoggingOut] = useState(false);
   const navigate = useNavigate();
@@ -49,6 +50,9 @@ const NavBar = () => {
 
   return (
     <header>
+
+      <img className='logo-container' src="https://www.fontys.nl/upload/eebc050a-a660-4c8e-8da8-0ed755d3c1dc_LOGO-FON-en-PAY-OFF-CARD-PAARS-WIT-RGB-pay-off-RZ.png" alt="fontys-logo"  height="70px" />
+
       <div className="container">
         <div className="items">
           <h1>CBBD Competency Game</h1>
